24929273bff8a36007a81a6c94fc976dfdd4dd7f
Akhlaque Ansari <124654189+akhl1234@users.noreply.github.com>
2024-02-09 16:35:01 +0000

    variables and datatypes added

---
+++ Basics/Data_Types.js
@@ -0,0 +1,27 @@
+// Data types describe the different types or kinds of data that we're gonna be working with and storing in variables. In Javascript, there are five basic, or primitive, types of data. The five most basic types of data are strings, numbers, booleans, undefined, and null.
+
+// String data type
+var name = 'John';
+
+// Number data type
+var age = 25;
+
+// Boolean data type
+var isStudent = true;
+
+// Array data type
+var fruits = ['apple', 'banana', 'orange'];
+
+// Object data type
+var person = {
+  name: 'John',
+  age: 25,
+  isStudent: true
+};
+
+// Print the values and types of the variables to the console
+console.log('Name:', name, typeof name);
+console.log('Age:', age, typeof age);
+console.log('Is Student:', isStudent, typeof isStudent);
+console.log('Fruits:', fruits, typeof fruits);
+console.log('Person:', person, typeof person);
---
+++ Basics/Variables.js
@@ -0,0 +1,17 @@
+// Declare a variable named 'name' and assign it the value 'John'
+var name = 'John';
+
+// Declare a variable named 'age' and assign it the value 25
+var age = 25;
+
+// Declare a variable named 'isStudent' and assign it the value true
+var isStudent = true;
+
+// Print the value of the 'name' variable to the console
+console.log('Name:', name);
+
+// Print the value of the 'age' variable to the console
+console.log('Age:', age);
+
+// Print the value of the 'isStudent' variable to the console
+console.log('Is Student:', isStudent);
