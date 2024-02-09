0388d2c10bf2cf2e5b8e2e9fceb4a4bd27a095d4
Akhlaque Ansari <124654189+akhl1234@users.noreply.github.com>
2024-02-09 17:02:01 +0000

    types conversion

---
+++ Basics/types_conversion.js
@@ -0,0 +1,18 @@
+// String Concatenation:
+let num = 10;
+let str = "The number is " + num; // Implicit conversion of num to string
+console.log(str);
+
+// Mathematical Operations:
+let result = "10" * 2;
+console.log(result);
+
+// Converting to Number:
+let strng = "42";
+let numbr = Number(strng);
+console.log(numbr); 
+
+// Converting to Boolean:
+let value = 0;
+let bool = Boolean(value);
+console.log(bool); 
