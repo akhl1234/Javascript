74a47bc0722694e0442fb2cc3d9a43056f46d8d8
Akhlaque Ansari <124654189+akhl1234@users.noreply.github.com>
2024-02-13 11:54:32 +0000

    Comaprisons in js comit changes

---
+++ Basics/Comparisions.js
@@ -0,0 +1,11 @@
+// Equal (==): Checks if two values are equal, performing type coercion if necessary.
+let A = 5 == 5; B = "5" == 5; C = 4 == 3
+console.log(A, B, C);
+//Strict Equal (===): Checks if two values are equal without type coercion.
+let nums_str = 5==="5"; Nums = 5===5 // this one is best for comparison any code;
+console.log(nums_str,Nums);
+// Greater Than (>), Greater Than or Equal To (>=), Less Than (<), Less Than or Equal To (<=):
+console.log(5 > 3);  
+console.log(5 >= 5); 
+console.log(5 < 3);
+console.log(5 <= 5); 
