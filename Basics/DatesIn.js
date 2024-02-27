e8f479b81a1fd9bfb71e6bf6c0f07250c5d3f5ff
Akhlaque Ansari <124654189+akhl1234@users.noreply.github.com>
2024-02-27 03:29:37 +0000

    DateIn Added config changes

---
+++ Basics/DatesIn.js
@@ -0,0 +1,5 @@
+const Now = new Date() //when you console this it will print right time..
+const Year = Now.getFullYear(); // show current year
+const months = Now.getMonth(); // January is 0, December is 11
+const Day = Now.getUTCDate(); // current date of month
+const Types_change=Now.toDateString(); //as a string representation
