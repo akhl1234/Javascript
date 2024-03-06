073897b32eb3ef5efd1baeb71030c1de0ce2d578
akhl1234 <shameer.alam2512@outlook.com>
2024-03-06 14:12:58 +0530

    Added JavaScript function files to the repository

---
+++ Functions/Intro.js
@@ -0,0 +1,10 @@
+let explanationOfFunction =
+  "What is the use of a function? Functions are self-contained modules of code that accomplish a specific task. \n They typically receive data as input, process it, and then return a result.\n Once a function is defined, it can be reused multiple times.";
+console.log(explanationOfFunction);
+
+// Let's Explore with some examples---
+function greet(Your_Name) {
+  console.log("Hello ", Your_Name + "\n How are you");
+}
+greet("My_freind");
+greet("Repaet with Another String/Name");
