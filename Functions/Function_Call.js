0e0defd62dfee26302dd0b97787736476483768d
akhl1234 <shameer.alam2512@outlook.com>
2024-03-10 09:16:28 +0530

    call stack method are added

---
+++ Functions/Function_Call.js
@@ -0,0 +1,13 @@
+const What_func_call =
+  "It’s like dialing a phone number to connect with someone—when you call a function, you activate its code and make it run! ";
+
+function Add(a, b) {
+  return a + b;
+}
+
+function Average_of_twoNum(a, b) {
+  return (a + b) / 2;
+}
+
+let x = Average_of_twoNum(15, 35);
+console.log(x);
